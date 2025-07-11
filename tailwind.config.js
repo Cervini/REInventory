09f7df18e15262d746c4875f6d35e475219f3975
Cervini <simonecervini99@gmail.com>
2025-07-12 01:00:49 +0200

    feat: fetch items and positions from Firebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'bg-red-500',
+    'bg-blue-500',
+    'bg-gray-400',
+    'bg-yellow-400',
+    'bg-green-600',
+    'bg-purple-600',
+  ],
   theme: {
     extend: {},
   },
